b14ef93c3260893d1d53139bad94d20158b6ad9d
Tynan DeBold <tynan.debold@tradeshift.com>
2019-02-12 10:51:43 +0100

    drag and drop functionality added

--- file-uploader/file-uploader.js
+++ file-uploader/file-uploader.js
@@ -3,6 +3,7 @@ import { defaultState } from './states/default.js';
 import { uploadingState } from './states/uploading.js';
 import { uploadFile } from './utils/uploadFile.js';
 import { calcFileSize } from './utils/calcFileSize.js';
+import { validateFileType } from './utils/validateFileType.js';
 
 const [
   $template,
@@ -25,17 +26,18 @@ class FileUploader extends Base(HTMLElement, 'FileUploader') {
   
   constructor() {
     super();
-    this.filetypes = this.getAttribute('filetypes');    
+    this.filetypes = this.getAttribute('filetypes');
 
     this.styles('/file-uploader/file-uploader.css');
     this.template(`
-    <div class="file-upload-wrapper">
+    <div class="file-upload-wrapper drop">
       ${defaultState(this.filetypes)}
     </div>
     <div class="file-card-wrapper"></div>`, 
     $template);
 
     this.fileType;
+    this.dragCounter = 0;
     this.formData = new FormData();
     this.state = this.getAttribute('state');
     this.endpoint = this.getAttribute('endpoint');
@@ -43,12 +45,21 @@ class FileUploader extends Base(HTMLElement, 'FileUploader') {
     this.handleClose = this.handleClose.bind(this);
     this.handleUpload = this.handleUpload.bind(this);
     this.handleDownload = this.handleDownload.bind(this);
-    this.resetFileInfo = this.resetFileInfo.bind(this);
+    this.resetFileFormats = this.resetFileFormats.bind(this);
+    this.dragEnterHandler = this.dragEnterHandler.bind(this);
+    this.dragOverHandler = this.dragOverHandler.bind(this);
+    this.dragLeaveHandler = this.dragLeaveHandler.bind(this);
+    this.dropHandler = this.dropHandler.bind(this);
+    this.fileUploadWrapper = this.shadowRoot.querySelector('.file-upload-wrapper');
   }
   attributeChangedCallback(name, oldValue, newValue) {
     // attach event listener when component first loads, otherwise, attach it in the handleClose fn
     if (newValue === 'upload' && oldValue === null) {
       this.shadowRoot.querySelector('input').addEventListener('change', this.handleUpload);
+      this.fileUploadWrapper.addEventListener('dragenter', this.dragEnterHandler, false);
+      this.fileUploadWrapper.addEventListener('dragover', this.dragOverHandler, false);
+      this.fileUploadWrapper.addEventListener('dragleave', this.dragLeaveHandler, false);
+      this.fileUploadWrapper.addEventListener('drop', this.dropHandler, false);
     }
 
     if (newValue === 'uploading') {
@@ -118,6 +129,7 @@ class FileUploader extends Base(HTMLElement, 'FileUploader') {
     }
     
     uploadFile(this, $fileCard, file);
+    this.resetFileFormats();
   }
   handleClose(e) {
     if (e.target.classList.contains('file-card--close')) {
@@ -131,9 +143,47 @@ class FileUploader extends Base(HTMLElement, 'FileUploader') {
     this.fileType = this.shadowRoot.querySelector('.filetype').innerHTML;
     this.shadowRoot.querySelector('.filetype').innerHTML = '<small class="message">Click to download</span>';
   }
-  resetFileInfo() {
-    this.shadowRoot.querySelector('.filetype').innerHTML = this.fileType;
+  resetFileFormats() {
+    this.shadowRoot.querySelector('.file-formats').style.color = '#565656';
+    this.shadowRoot.querySelector('.info-icon').style.fill = '#00B0FF';
+  }
+  dragEnterHandler(e) {
+    e.preventDefault();
+    this.dragCounter++;
+    if (this.dragCounter > 0) {
+      this.fileUploadWrapper.classList.add('dragover');
+    }
+  }
+  dragOverHandler(e) {
+    e.preventDefault();
+  }
+  dragLeaveHandler(e) {
+    e.preventDefault();
+    this.dragCounter--;
+    if (this.dragCounter === 0) {
+      this.fileUploadWrapper.classList.remove('dragover');
+    }
+  }
+  dropHandler(e) {
+    e.preventDefault();
+    this.fileUploadWrapper.classList.remove('dragover');
+    const dt = e.dataTransfer;
+    const files = dt.files;
+    const isValidFileType = validateFileType(files[0], this.filetypes);    
+
+    if (isValidFileType) {
+      const event = {
+        type: 'change',
+        target: {
+          files
+        }
+      };
+      this.handleUpload(event);
+      this.resetFileFormats();
+    } else {
+      this.shadowRoot.querySelector('.file-formats').style.color = '#FF1744';
+      this.shadowRoot.querySelector('.info-icon').style.fill = '#FF1744';
+    }
   }
 }
-
 customElements.define('ts-file-uploader', FileUploader);
